f642b5c58596654a13aa44deb61665eb4f54e5f0
Lizzie <lizziewrab@me.com>
2022-08-30 18:08:39 -0500

    commit tree aa88f1069d88ba8116daad3254c24bfc96d64789

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters 
+  has_many :actors, through: :shows 
+  belongs_to :network 
+
+  def actors_list
+    self.characters.map do |c|
+        c.actor.full_name 
+    end
+  end
 end
